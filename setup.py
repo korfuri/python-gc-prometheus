8fd86cbce96ac4a4aa536a59aa36c8df27370366
Uriel Corfa <korfuri@gmail.com>
2015-04-25 11:52:59 -0700

    Add the first version of the code. This doesn't include any profiling of code performance.

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup
+
+LONG_DESCRIPTION = """GC-Prometheus
+
+Exports metrics about the Python garbage collector for Prometheus.io.
+
+See https://github.com/korfuri/python-gc-prometheus for usage
+instructions.
+"""
+
+setup(
+    name="gc-prometheus",
+    version="0.0.1",
+    author="Uriel Corfa",
+    author_email="uriel@corfa.fr",
+    description=(
+        "Exports garbage collection metrics for Prometheus.io."),
+    license="Apache",
+    keywords="python gc monitoring prometheus",
+    url="http://github.com/korfuri/python-gc-prometheus",
+    packages=["gc_prometheus"],
+    test_suite="tests",
+    long_description=LONG_DESCRIPTION,
+    install_requires=[
+        "prometheus_client>=0.0.9",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "Topic :: System :: Monitoring",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+)
---
+++ tests/test_gc_stats.py
@@ -0,0 +1,42 @@
+import gc
+import unittest
+from prometheus_client import REGISTRY
+import gc_prometheus
+
+
+class TestGcPrometheus(unittest.TestCase):
+    def test_enabled(self):
+        self.assertEqual(1, REGISTRY.get_sample_value('python_gc_enabled'))
+        try:
+            gc.disable()
+            self.assertEqual(0, REGISTRY.get_sample_value('python_gc_enabled'))
+        finally:
+            gc.enable()
+
+    def test_debug(self):
+        self.assertEqual(0, REGISTRY.get_sample_value('python_gc_debug'))
+        try:
+            gc.set_debug(gc.DEBUG_STATS)
+            self.assertEqual(gc.DEBUG_STATS, REGISTRY.get_sample_value('python_gc_enabled'))
+        finally:
+            gc.set_debug(0)
+
+    def test_thresholds(self):
+        self.assertTrue(REGISTRY.get_sample_value('python_gc_threshold', labels={'generation': '0'}) is not None)
+        self.assertTrue(REGISTRY.get_sample_value('python_gc_threshold', labels={'generation': '1'}) is not None)
+        self.assertTrue(REGISTRY.get_sample_value('python_gc_threshold', labels={'generation': '2'}) is not None)
+        original_thresholds = gc.get_threshold()
+        try:
+            gc.disable()
+            gc.set_threshold(42, 43, 44)
+            self.assertEqual(42, REGISTRY.get_sample_value('python_gc_threshold', labels={'generation': '0'}))
+            self.assertEqual(43, REGISTRY.get_sample_value('python_gc_threshold', labels={'generation': '1'}))
+            self.assertEqual(44, REGISTRY.get_sample_value('python_gc_threshold', labels={'generation': '2'}))
+        finally:
+            gc.set_threshold(*original_thresholds)
+            gc.enable()
+
+
+
+if __name__ == '__main__':
+    unittest.main()
