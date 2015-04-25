11ad9e9265832b1f5cac499f9482d1f9ede2cb05
Uriel Corfa <uriel@corfa.fr>
2015-04-25 12:18:24 -0700

    Do not run tests that rely on python3.3+ on earlier versions.

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -6,18 +6,20 @@ if sys.version_info >= (3, 3):
     import gc_prometheus.profile
 
 
-class TestGcProfile(unittest.TestCase):
-    def test_import_pre_py33(self):
-        if sys.version_info < (3, 3):
+if sys.version_info < (3, 3):
+    class TestImports(unittest.TestCase):
+        def test_import_pre_py33(self):
             with self.assertRaises(ImportError):
                 import gc_prometheus.profile
 
-    def test_collection_time(self):
-        current_total = REGISTRY.get_sample_value(
-            'python_gc_collection_process_time_total_s')
-        gc.collect()
-        self.assertTrue(current_total < REGISTRY.get_sample_value(
-            'python_gc_collection_process_time_total_s'))
+else:
+    class TestGcProfile(unittest.TestCase):
+        def test_collection_time(self):
+            current_total = REGISTRY.get_sample_value(
+                'python_gc_collection_process_time_total_s')
+            gc.collect()
+            self.assertTrue(current_total < REGISTRY.get_sample_value(
+                'python_gc_collection_process_time_total_s'))
 
 
 if __name__ == '__main__':
